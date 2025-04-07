092af0483f2c0c394d53eea52326ff90ee4df7c3
Amiroo <amir.charkhand@outlook.com>
2025-04-07 11:01:00 +0330

    DEVELOPMENT: Register new user development

--- RealEstateApp/ViewModels/RegisterNewUserViewModel.cs
+++ RealEstateApp/ViewModels/RegisterNewUserViewModel.cs
@@ -1,10 +1,61 @@
-﻿namespace RealEstateApp.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using RealEstateApp.Models;
+using RealEstateApp.Services;
+
+namespace RealEstateApp.ViewModels
 {
     public partial class RegisterNewUserViewModel : BaseViewModel
     {
-        public RegisterNewUserViewModel() 
+        private readonly IServiceProvider _serviceProvider;
+
+        private UserApiService UserApiService => _serviceProvider.GetRequiredService<UserApiService>();
+
+        [ObservableProperty]
+        private string _name;
+        [ObservableProperty]
+        private string _email;
+        [ObservableProperty]
+        private string _password;
+        [ObservableProperty]
+        private string _phone;
+
+        public RegisterNewUserViewModel(IServiceProvider serviceProvider)
         {
             Title = "Sign Up";
+            _serviceProvider = serviceProvider;
+        }
+
+        [RelayCommand]
+        private async Task RegisterUser()
+        {
+            if (string.IsNullOrWhiteSpace(Name) || string.IsNullOrWhiteSpace(Email) || string.IsNullOrWhiteSpace(Password) || string.IsNullOrWhiteSpace(Phone))
+            {
+                await Shell.Current.DisplayAlert("Error", "All fields are required.", "OK");
+                return;
+            }
+            try
+            {
+                IsBusy = true;
+                var newUser = new NewUser(Name, Email, Password, Phone);
+                await UserApiService.RegisterUserAsync(newUser);
+                await Shell.Current.DisplayAlert("Success", "User registered successfully. You can login now", "OK");
+                await GoToLoginPage();
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Error", $"Registration failed: {ex.Message}", "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        [RelayCommand]
+        private async Task GoToLoginPage()
+        {
+            await Shell.Current.GoToAsync("..", true);
         }
     }
 }
