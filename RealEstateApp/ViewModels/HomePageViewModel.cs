bb6842625a3dea68e932d228be302e5e832995a9
Amiroo <amir.charkhand@outlook.com>
2025-04-16 13:36:58 +0330

    DEVELOPMENT: Home page development

--- RealEstateApp/ViewModels/HomePageViewModel.cs
+++ RealEstateApp/ViewModels/HomePageViewModel.cs
@@ -1,7 +1,101 @@
-﻿
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using RealEstateApp.Exceptions;
+using RealEstateApp.Models;
+using RealEstateApp.Services;
+using System.Collections.ObjectModel;
+
 namespace RealEstateApp.ViewModels
 {
-    public class HomePageViewModel : BaseViewModel
+    public partial class HomePageViewModel : BaseViewModel
     {
+        private readonly LoginInfoStorageService _loginInfoStorageService;
+        private readonly IServiceProvider _serviceProvider;
+
+        private CategoryApiService CategoryApiService => _serviceProvider.GetRequiredService<CategoryApiService>();
+        private PropertyApiService PropertyApiService => _serviceProvider.GetRequiredService<PropertyApiService>();
+
+        [ObservableProperty]
+        private string _username;
+
+        public ObservableCollection<Category> Categories { get; init; } = new ObservableCollection<Category>();
+        public ObservableCollection<Property> TrendingProperties { get; init; } = new ObservableCollection<Property>();
+
+        public HomePageViewModel(LoginInfoStorageService loginInfoStorageService, IServiceProvider serviceProvider)
+        {
+            _loginInfoStorageService = loginInfoStorageService;
+            _serviceProvider = serviceProvider;
+        }
+
+        [RelayCommand]
+        private async Task SetUserInfo()
+        {
+            var loginInfo = await _loginInfoStorageService.GetLoginInfoAsync();
+            if (loginInfo != null)
+                Username = $"Welcome {loginInfo.UserName}";
+            else
+            {
+                await Shell.Current.DisplayAlert("Error", "User not found! You need to Login first", "OK");
+                await Shell.Current.GoToAsync("//LoginPage");
+            }
+        }
+
+        [RelayCommand]
+        private async Task SetCategories()
+        {
+            if (Categories.Count > 0)
+                return;
+
+            try
+            {
+                IsBusy = true;
+                var categories = await CategoryApiService.GetCategoriesAsync();
+                foreach (var category in categories)
+                    Categories.Add(category);
+            }
+            catch (Exception ex) when (ex is not UserLoginException)
+            {
+                // Handle other exceptions, such as network errors or server errors
+                await Shell.Current.DisplayAlert("Error", $"Couldn't retrieve Categories from server {ex.Message}", "OK");
+            }
+            catch (UserLoginException ex)
+            {
+                await Shell.Current.DisplayAlert("Error", $"User is not loged in. Please Login first {ex.Message}", "OK");
+                await Shell.Current.GoToAsync("//LoginPage");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        [RelayCommand]
+        private async Task SetTrendingProperties()
+        {
+            if (TrendingProperties.Count > 0)
+                return;
+
+            try
+            {
+                IsBusy = true;
+                var trendingProperties = await PropertyApiService.GetTrendingPropertiesAsync();
+                foreach (var property in trendingProperties)
+                    TrendingProperties.Add(property);
+            }
+            catch (Exception ex) when (ex is not UserLoginException)
+            {
+                // Handle other exceptions, such as network errors or server errors
+                await Shell.Current.DisplayAlert("Error", $"Couldn't retrieve Trending Properties from server {ex.Message}", "OK");
+            }
+            catch (UserLoginException ex)
+            {
+                await Shell.Current.DisplayAlert("Error", $"User is not loged in. Please Login first {ex.Message}", "OK");
+                await Shell.Current.GoToAsync("//LoginPage");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
     }
 }
--- RealEstateApp/Views/HomePage.xaml.cs
+++ RealEstateApp/Views/HomePage.xaml.cs
@@ -4,9 +4,21 @@ namespace RealEstateApp.Views;
 
 public partial class HomePage : ContentPage
 {
-	public HomePage(HomePageViewModel viewModel)
+	private readonly HomePageViewModel _viewModel;
+    public HomePage(HomePageViewModel viewModel)
 	{
 		InitializeComponent();
         BindingContext = viewModel;
+        _viewModel = viewModel;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await Task.WhenAll(
+            _viewModel.SetCategoriesCommand.ExecuteAsync(null),
+            _viewModel.SetUserInfoCommand.ExecuteAsync(null),
+            _viewModel.SetTrendingPropertiesCommand.ExecuteAsync(null)
+        );
     }
 }
