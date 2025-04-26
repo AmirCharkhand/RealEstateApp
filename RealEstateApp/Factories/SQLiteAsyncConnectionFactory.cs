50ac49e619a75c52704337ccc8103b9758568c04
Amiroo <amir.charkhand@outlook.com>
2025-04-26 16:52:25 +0330

    DEVELOPMENT: Generic SQLite service code development

---
+++ RealEstateApp/Factories/SQLiteAsyncConnectionFactory.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace RealEstateApp.Factories
+{
+    public static class SQLiteAsyncConnectionFactory<T> where T : class, new()
+    {
+        public static async Task<SQLiteAsyncConnection> CreateConnectionAsync()
+        {
+            var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "properties.db");
+            var connection = new SQLiteAsyncConnection(dbPath);
+            await connection.CreateTableAsync<T>();
+            return connection;
+        }
+    }
+}
---
+++ RealEstateApp/Services/SqliteService.cs
@@ -0,0 +1,45 @@
+﻿using RealEstateApp.Factories;
+using SQLite;
+
+namespace RealEstateApp.Services
+{
+    public class SqliteService<T> where T : class, new()
+    {
+        private SQLiteAsyncConnection? _connection;
+
+        private async Task<SQLiteAsyncConnection> GetConnectionAsync()
+        {
+            _connection ??= await SQLiteAsyncConnectionFactory<T>.CreateConnectionAsync();
+            return _connection;
+        }
+
+        public async Task<List<T>> GetAllItemsAsync()
+        {
+            var connection = await GetConnectionAsync();
+            return await connection.Table<T>().ToListAsync();
+        }
+        public async Task<T?> GetItemAsync(int id)
+        {
+            var connection = await GetConnectionAsync();
+            return await connection.FindAsync<T>(id);
+        }
+        public async Task SaveItemAsync(T item)
+        {
+            var connection = await GetConnectionAsync();
+            var result = await connection.InsertAsync(item);
+            if (result != 1)
+            {
+                throw new Exception("Failed to save item.");
+            }
+        }
+        public async Task DeleteItemAsync(T item)
+        {
+            var connection = await GetConnectionAsync();
+            var result = await connection.DeleteAsync(item);
+            if (result != 1)
+            {
+                throw new Exception("Failed to delete item.");
+            }
+        }
+    }
+}
